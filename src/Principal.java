e4ffaa80653666be0ca32bd30fe09f633127a8a7
guille <guille@DESKTOP-PBFV40H>
2023-12-02 16:18:47 -0400

    Se agrega algoritmo de que da la secuencia fibonacci para un n pedido por teclado. Archivo: Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Sucesion de fibonacci");
+        System.out.print("Coloque el numero de terminos de fibonacci: ");
+        Scanner teclado = new Scanner(System.in);
+        int i = teclado.nextInt();
+        int temp ,sig =1, ant = 0;
+        for (int x = 0 ; x<i ; ++x) {
+            System.out.print(ant +", ");
+            temp = sig;
+            sig = sig +ant;
+            ant = temp;
+        }
+        System.out.println();
     }
     
 }
